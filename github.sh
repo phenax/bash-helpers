f2815beb5d2c14b64a649f1d416010294c9fa130
Akshay Nair <phenax5@gmail.com>
2019-02-09 21:38:46 +0530

    Adds git-open-pr to open a pull request compare page (github specific)

--- github.sh
+++ github.sh
@@ -4,10 +4,23 @@ import-module "utils.sh";
 
 git-open() {
   local url=$(git-get-remote-url $1);
-  if [ "$url" == "" ];
-  then
+  if [ "$url" == "" ]; then
     echo "No remote url found for $1";
   else
+    xdg-open "$url";
+  fi;
+}
+
+git-open-pr() {
+  local remote=${1:-"origin"};
+  local targetBranch=${2:-"master"};
+  local localBranch=$(git-get-branch-name);
+  local url=$(git-get-remote-url $remote);
+
+  if [ "$url" == "" ]; then
+    echo "No remote url found for $1";
+  else
+    url="$url/compare/$targetBranch...$localBranch";
     xdg-open "$url";
   fi;
 }
--- tests/github.test.sh
+++ tests/github.test.sh
@@ -4,7 +4,7 @@ import-module "github.sh";
 
 test "git-open"; {
 
-  # Should trigger xdg-open with given url
+  # Should trigger xdg-open with repo url
   git-open;
   assertEq "https://url.com/repo" "$mockLastOpenedUrl";
 
@@ -18,3 +18,26 @@ test "git-open"; {
   assertEq "No remote url found for hello" "$res";
   assertEq "" "$mockLastOpenedUrl";
 }
+
+test "git-open-pr"; {
+
+  # Should trigger xdg-open with repo's pr url
+  git checkout my-branch > /dev/null;
+  git-open-pr;
+  assertEq "https://url.com/repo/compare/master...my-branch" "$mockLastOpenedUrl";
+
+  # Should open pr url with upstream branch
+  git-open-pr upstream;
+  assertEq "https://url.com/upstream-repo/compare/master...my-branch" "$mockLastOpenedUrl";
+
+  # Should trigger xdg-open with upstream url
+  git-open-pr upstream develop;
+  assertEq "https://url.com/upstream-repo/compare/develop...my-branch" "$mockLastOpenedUrl";
+
+  mockLastOpenedUrl="";
+  # Should return exit code 1, if repo was not found
+  local res=$(git-open-pr hello);
+  assertEq "No remote url found for hello" "$res";
+  assertEq "" "$mockLastOpenedUrl";
+}
+
