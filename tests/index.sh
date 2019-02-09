95b0f0472bbbcf563bce5d37fc26f69d634ec8da
Akshay Nair <phenax5@gmail.com>
2019-02-09 19:43:48 +0530

    Adds return code 1 for failed tests

--- tests/index.sh
+++ tests/index.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e;
 
 # Returns absolute directory of a file/dir path
 absolute-dir() { echo $(dirname "$(realpath $1)"); }
@@ -8,7 +9,9 @@ import-module() { source "$(absolute-dir $0)/$1"; }
 
 assertEq() {
   [ "$1" == "$2" ] || {
-    echo "Test Failed! Expected $1, but got $2";
+    echo "Test Failed! Expected \"$1\", but got \"$2\"";
+    let errors+=1;
+    return 1;
   }
 } 
 
@@ -16,3 +19,5 @@ assertEq() {
 ls $(absolute-dir $0) |
   grep ".test.sh" |
   while read file; do import-module $file; done;
+
+echo "All tests passed successfully!";
--- tests/utils.test.sh
+++ tests/utils.test.sh
@@ -4,4 +4,5 @@ echo "cool";
 
 import-module "../utils.sh";
 
-assertEq "wow" "master";
+assertEq "1" "1";
+assertEq $(get-get-origin-url) "master";
